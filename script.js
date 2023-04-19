cf671ff578610a42bae284b1f287a511578f4902
unknown <brhouma85@gmail.com>
2023-04-19 12:50:04 -0700

    adding variables length and numbers

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// variable for 8 character string
+var length = 8;
+// variable numbers to be used
+var numbers = '0123456789';
+
+
 
 function generatePassword() {
   
